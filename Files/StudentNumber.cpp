0117cf556b8cb95d330f4fea2f0af6a61ec47ab7
AntonieSmith <115539303+AntonieSmith-Lab@users.noreply.github.com>
2024-08-31 15:34:59 +0200

    2024_08_31_15h30

---
+++ Files/StudentNumber.cpp
@@ -0,0 +1,18 @@
+//------------------------------------------------------------------------------//
+/// STUDENT AND EVALUATION DETAILS - Complete
+//------------------------------------------------------------------------------//
+/// Student Number:
+/// Date:
+/// Campus:
+/// Department:
+/// Venue:
+//------------------------------------------------------------------------------//
+
+//------------------------------------------------------------------------------//
+/// EVALUATION REQUIREMENTS
+//------------------------------------------------------------------------------//
+/// a.	Remember to save your work on the PC “D: Drive” and save regularly throughout the evaluation.
+/// b.	Do not modify the given code in the “.cpp” file except for implementing the requested functions as required.
+/// c.	Use the exact function names and parameters as used in the in the “.cpp” file function prototypes and main function.
+/// d.	Complete the C++ functions below the main function in each comment block as shown.
+//------------------------------------------------------------------------------//
